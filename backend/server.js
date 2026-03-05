e1e7f7a3f226d448ae66ca13de5bf22199244d75
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-03-05 21:01:51 +0200

    Middleware folder + health check endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,17 @@ import dotenv from 'dotenv'
 // Load environment variables
 dotenv.config()
 
+const PORT = process.env.PORT
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/health-check', (req, res) => {
+    res.send({
+        status: 'ok',
+        message: 'API up and running'
+    })
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`))
